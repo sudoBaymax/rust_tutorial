69bf5cc78cc50787d1d090addca6e0bace62e3b1
sudoBaymax <jatoujoseph@gmail.com>
2025-04-26 10:15:10 -0400

    completed primitive data types lesson, played around with rust

--- primitiveData/src/main.rs
+++ primitiveData/src/main.rs
@@ -16,14 +16,34 @@ fn main(){
     println!("Signed Integer: {}", x);
     println!("Unsigned Integer: {}", y);
 
-    //diff bet i32 (32 bits) and i64 (64 bits)
-    // range : 
-    // i32 - 2147483647
-    // i64 - 9223372036854775807
+// diff bet i32 (32 bits) and i64 (64 bits)
+// range : 
+// i32 - 2147483647
+// i64 - 9223372036854775807
     let e: i32 = 2147483647;
     let i: i64 = 9223372036854775807;
     println!("Maximum value of i32: {}", e);
     println!("Maximum value of i64: {}", i);
+// ==========================================
+// Floats [Floating Point Types]
+// f32, f64
+    let pi: f64 = 3.14;
+    println!("Value of pi: {}", pi);
+// Boolean Values: true, false
+    let is_snowing: bool = true;
+    println!("is it snowing ? {}", is_snowing);
+//  Character Type - char
+    let letter: char = 'a';
+    println!("First letter of the alphabet: {}", letter);
+    
+
+//  i32, i64, u32, u64, f32, f64, char, bool
+
+    let number_one: i64 = 124;
+    let number_two: i64 = 24;
+
+    println!("Number one is {} and number two is {}", number_one, number_two);
+    println!("{} + {} = {}", number_one, number_two, number_one+number_two)
 
 }
 
