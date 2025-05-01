216c308ecdc93a36516a691ce7c007f6f46a8915
sudoBaymax <jatoujoseph@gmail.com>
2025-04-30 22:35:25 -0400

    Add mutable string example and implement even_or_odd function

--- compoundData/src/main.rs
+++ compoundData/src/main.rs
@@ -37,5 +37,44 @@ fn main() {
     // Strings Vs String Slices (&str)
     // Strings [ growable, mutable, owned string type ] 
 
+    // All data types in rust are by default immutable (meaning you can't change the value)
+    // To change this, we can do the following:
+    let mut stone_cold: String = String::from("Hell, "); // must have "mut" included to make it mutable
+    println!("Stone COld Says: {}", stone_cold); 
+    stone_cold.push_str("Yeah!");
+    println!("Stone Cold Says: {}", stone_cold);
+
+    //  B- &str (String Slice)
+    let string: String = String::from("Hello, World");
+    let slice: &str = &string[0..5];
+    println!("Slice Value: {}", slice);
+    even_or_odd(&[1,2,3,4,5]);
 
 }
+
+fn even_or_odd(numbers: &[i32]) {
+    for number in numbers {
+        if number % 2 == 0 {
+            println!("{} is Even", number);
+        }
+        else {
+            println!("{} is odd", number);
+        }
+
+    }
+}
+
+
+// Generate Report(profiles: &[(NAME, [Grades])]);
+// for profile in profiles:
+//          average = sum(Grades) / 2
+            // student = profile[0]
+            // Grades = profile[1]
+
+            // if average >= 90: A
+            // if average >= 80 and grade <= 89: B
+            // if average >= 70 and grade <= 79: C
+            // if average >= 60 and grade <= 69: D
+            // if average < 60: F
+
+        // result: String = String::from("Student: {} \nGrades: {} \nAverage: {} \nLetterGrade", student, Grades, average, letter);    
